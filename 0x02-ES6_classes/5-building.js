2d89fad0e3fad78d8420e23bac962e4022ae6b2a
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-03-07 18:05:11 +0100

    building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('evacuationWarningMessage not implemented');
+  }
+}
+
+export default Building;
