e87495c15ad6e8c04c7c3ff17fa7530fd30d063e
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-03-12 17:27:37 +0100

    map

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,16 @@
-export default function groceriesList() {
+const groceriesList = () => {
   const newMap = new Map();
-  newMap.set('Apple', 10);
-  newMap.set('Tomatoes', 10);
-  newMap.set('Pasta', 1);
-  newMap.set('Rice', 1);
-  newMap.set('Banana', 5);
-
+  const items = {
+    Apples: 10,
+    Tomatoes: 10,
+    Pasta: 1,
+    Rice: 1,
+    Banana: 5,
+  };
+  for (const key of Object.keys(items)) {
+    newMap.set(key, items[key]);
+  }
   return newMap;
-}
+};
+
+export default groceriesList;
