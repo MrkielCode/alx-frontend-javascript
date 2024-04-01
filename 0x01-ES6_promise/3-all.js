0cd50356afd6128348c62ef6d1d8769af8fd2dfd
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-04-01 17:26:45 +0100

    all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoRes, userRes]) => {
+      console.log(`${photoRes.body} ${userRes.firstName} ${userRes.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
